8f3920a9a7fa190c4f7db819d5f93e070d62b004
Bob <rwillford0719@gmail.com>
2022-07-31 14:45:45 -0500

    added everything befor heroku deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -20,12 +25,12 @@ const httpLink = createHttpLink({
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
-    header: {
+    headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : '',
     },
-  }
-})
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
@@ -36,36 +41,34 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className='flex-column justify-flex-start min-100-vh'>
+        <div className="flex-column justify-flex-start min-100-vh">
           <Header />
-          <div className='container'>
+          <div className="container">
             <Routes>
-              <Route
-                path="/"
-                element={<Home />}
+              <Route 
+                path="/" 
+                element={<Home />} 
               />
-              <Route
-                path="/login"
-                element={<Login />}
+              <Route 
+                path="/login" 
+                element={<Login />} 
               />
-              <Route
-                path="/signup"
-                element={<Signup />}
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
               />
-              <Route path="/profile">
-                <Route path=":username" element={<Profile />} />
-                <Route path="" element={<Profile />} />
-              </Route>
-              <Route
-                path="/thought/:id"
-                element={<SingleThought />}
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
               />
-
-              <Route
-                path="*"
-                element={<NoMatch />}
+              <Route 
+                path="/thought/:id" 
+                element={<SingleThought />} 
               />
-
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
             </Routes>
           </div>
           <Footer />
